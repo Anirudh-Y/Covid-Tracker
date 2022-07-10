3d2e415926909ec7762935664058b2c57829a0e1
Anirudh <anirudhy@kgpian.iitkgp.ac.in>
2022-07-10 18:03:52 +0530

    Updates

---
+++ precache-manifest.c33b36ebc66f37b033fe7effbadaee69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61ab2470b84c3b224f62ddfff736220",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "d9c4bba6b69942e7fb18",
+    "url": "/Covid-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "05fae6e595b106c851fc",
+    "url": "/Covid-Tracker/static/css/main.b9cd80a3.chunk.css"
+  },
+  {
+    "revision": "d9c4bba6b69942e7fb18",
+    "url": "/Covid-Tracker/static/js/2.c3990831.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-Tracker/static/js/2.c3990831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fae6e595b106c851fc",
+    "url": "/Covid-Tracker/static/js/main.a358aeb1.chunk.js"
+  },
+  {
+    "revision": "ae45445b70030a69d951",
+    "url": "/Covid-Tracker/static/js/runtime-main.d4cdb983.js"
+  }
+]);
